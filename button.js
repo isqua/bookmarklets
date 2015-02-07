132db365939b445dbaa852c6b436a822b8ed04d3
isqua <isqua@isqua.ru>
2015-02-07 22:36:49 +0300

    Сделать букмарклет

---
+++ button.js
@@ -0,0 +1,23 @@
+(function(){
+    var win = window.open(
+        'https://vk.com/share.php?url=' + encodeURIComponent(window.location.href),
+        'vksharebookmarklet',
+        [
+            'left=' + (screen.width/2 - 275),
+            'top=120',
+            'width=550',
+            'height=330',
+            'menubar=0',
+            'toolbar=0',
+            'location=0',
+            'directories=0',
+            'resizable=1',
+            'scrollbars=1',
+            'personalbar=0'
+        ].join(',')
+    );
+
+    if (win) {
+        win.focus();
+    }
+})();
