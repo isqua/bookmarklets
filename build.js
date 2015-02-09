bfc2754972e557482e42481117f95ff85dcc6415
isqua <isqua@isqua.ru>
2015-02-10 01:05:22 +0300

    Add build

---
+++ build.js
@@ -0,0 +1,34 @@
+var UglifyJS = require('uglify-js');
+var CleanCSS = require('clean-css');
+var fs = require('fs');
+
+var buttonJs = UglifyJS.minify('button.js');
+var styleCss = new CleanCSS().minify(fs.readFileSync('style.css').toString('utf8')).styles;
+
+buttonJs = buttonJs.code.replace(/"/gm, '\'');
+styleCss = '<style type="text/css">' + styleCss + '</style>';
+
+fs.readFile('index.html', function(err, data) {
+	var src = data.toString('utf8');
+	var result = '';
+
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	result = src
+		.replace(/(\r\n|\n|\r)/gm, '')
+		.replace(/\s+/gm, ' ')
+		.replace('{{buttonJs}}', buttonJs)
+		.replace('<link rel="stylesheet" href="style.css">', styleCss)
+		.replace(/>\s</gm, '><');
+
+	fs.writeFile('result.html', result, function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('done');
+		}
+	});
+});
