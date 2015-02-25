26805d61e1f7603f0a1045a88b9a665021a8dc48
isqua <isqua@isqua.ru>
2015-02-25 23:22:58 +0300

    Add Yandex.Metrika

--- build.js
+++ build.js
@@ -3,9 +3,11 @@ var CleanCSS = require('clean-css');
 var fs = require('fs');
 
 var buttonJs = UglifyJS.minify('button.js');
+var metrikaJs = UglifyJS.minify('metrika.js');
 var styleCss = new CleanCSS().minify(fs.readFileSync('style.css').toString('utf8')).styles;
 
 buttonJs = buttonJs.code.replace(/"/gm, '\'');
+metrikaJs = metrikaJs.code.replace(/"/gm, '\'');
 styleCss = '<style type="text/css">' + styleCss + '</style>';
 
 fs.readFile('index.html', function(err, data) {
@@ -21,6 +23,7 @@ fs.readFile('index.html', function(err, data) {
 		.replace(/(\r\n|\n|\r)/gm, '')
 		.replace(/\s+/gm, ' ')
 		.replace('{{buttonJs}}', buttonJs)
+		.replace('{{metrikaJs}}', metrikaJs)
 		.replace('<link rel="stylesheet" href="style.css">', styleCss)
 		.replace(/>\s</gm, '><');
 
