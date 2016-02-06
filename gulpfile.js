29a10a28980eb17f9111660c22d89b9159fc3e8c
isqua <isqua@isqua.ru>
2016-02-06 06:38:55 +0300

    Updated markup

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
-var gulp = require('gulp');
-var build = require('./build');
+const sections = require('./sections');
+const gulp = require('gulp');
+const src = 'src';
+const dest = 'build';
+const browserSync = require('browser-sync').create();
 
-gulp.task('html', function() {
-    return gulp.src('src/index.html')
-        .pipe(build())
-        .pipe(gulp.dest('dist'));
+const config = {
+    jade: {
+        src: 'src/index.jade',
+        dest: dest,
+        metrika: 28698696,
+        opts: {
+            locals: { sections: sections() }
+        }
+    }
+};
+
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: dest
+        }
+    });
 });
 
-gulp.task('default', [ 'html' ]);
+require('nbld/tasks/jade')(gulp, config.jade);
+
+gulp.task('default', [ 'jade:development', 'server' ], () => {
+    gulp.watch(config.jade.src, [ 'jade:development', browserSync.reload ]);
+});
+
+gulp.task('production', [ 'jade:production' ]);
---
+++ sections.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require('path');
+var uglify = require("uglify-js");
+var gutil = require("gulp-util");
+var bookmarklets = require('./src/index');
+
+function getContent(bookmarklets) {
+    return bookmarklets.map(getSection);
+}
+
+function getSection(section) {
+    return {
+        name: section.name,
+        id: section.id,
+        content: section.content.map(getBookmarklet(section.id))
+    };
+}
+
+function getBookmarklet(sectionId) {
+    return function (bookmarklet) {
+        return {
+            name: bookmarklet.name,
+            id: bookmarklet.id,
+            text: bookmarklet.text,
+            code: getCode(sectionId, bookmarklet.id)
+        };
+    };
+}
+
+function getCode(sectionId, bookmarkletId) {
+    var data = fs.readFileSync(getBookmarkletPath(sectionId, bookmarkletId));
+    var code = uglify(data.toString("utf8"));
+
+    gutil.log('added bookmark: ' + `${sectionId}/${bookmarkletId}`);
+
+    return 'javascript:' + encodeURIComponent(code);
+}
+
+function getBookmarkletPath(sectionId, bookmarkletId) {
+    return path.resolve(__dirname, `src/bm/${sectionId}/${bookmarkletId}.js`);
+}
+
+module.exports = function() {
+    return getContent(bookmarklets);
+};
