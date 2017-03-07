26f87a8bdc78c8f0cced61142f40ddef79cfb271
isqua <isqua@isqua.ru>
2017-03-07 09:44:15 +0300

    Fix hooray buttons

--- src/bm/github/shownothooray.js
+++ src/bm/github/shownothooray.js
@@ -1,6 +1,7 @@
 Array.prototype.filter.call(
-    document.querySelectorAll('.has-inline-notes:not(.open)'),
+    document.querySelectorAll('.outdated-comment.has-inline-notes:not(.open)'),
     function(inlineNote) { return !inlineNote.querySelector('.reaction-summary-item[value^="HOORAY"]') }
 )
     .map(function(inlineNote) { return inlineNote.querySelector('.show-outdated-button') })
+    .filter(Boolean)
     .forEach(function(button) { button.click() })
